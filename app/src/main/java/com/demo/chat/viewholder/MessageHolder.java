6e27e6dc2dd429441cfad108e41ac70465cb7574
Sahil Jain <sahiljain5040@gmail.com>
2018-06-09 13:02:41 +0530

    Reverting changes

--- app/src/main/java/com/demo/chat/viewholder/MessageHolder.java
+++ app/src/main/java/com/demo/chat/viewholder/MessageHolder.java
@@ -1,8 +1,10 @@
 package com.demo.chat.viewholder;
 
 import android.content.Context;
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -15,7 +17,7 @@ import butterknife.ButterKnife;
 public class MessageHolder extends RecyclerView.ViewHolder{
 
     @BindView(R.id.message_layout)
-    RelativeLayout messageLayout;
+    FrameLayout messageLayout;
 
     @BindView(R.id.message_text)
     TextView messageText;
@@ -50,14 +52,26 @@ public class MessageHolder extends RecyclerView.ViewHolder{
 
             messageLayout.setBackgroundResource(!isSameSender ? R.mipmap.bg_message : R.mipmap.bubble_final_other_user);
 
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                relativeParams.setMarginStart(leftMargin);
+                relativeParams.addRule(RelativeLayout.ALIGN_PARENT_END, 0);
+                relativeParams.addRule(RelativeLayout.ALIGN_PARENT_START);
+            }
             relativeParams.setMargins(leftMargin, topMargin, 0, bottomMargin);
+            relativeParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, 0);
             relativeParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
         } else {
             int rightMargin = mContext.getResources().getDimensionPixelSize(R.dimen.module_zero_dp);
-
             messageLayout.setBackgroundResource(!isSameSender ? R.mipmap.white_message_box : R.mipmap.bubble_message_white);
 
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                relativeParams.setMarginEnd(rightMargin);
+                relativeParams.addRule(RelativeLayout.ALIGN_PARENT_START, 0);
+                relativeParams.addRule(RelativeLayout.ALIGN_PARENT_END);
+            }
             relativeParams.setMargins(0, topMargin, rightMargin, bottomMargin);
+
+            relativeParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, 0);
             relativeParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
         }
         messageLayout.setLayoutParams(relativeParams);
