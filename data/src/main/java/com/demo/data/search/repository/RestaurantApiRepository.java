1642b1c745f07c1569f74ad3c94666bc05f66028
Sahil Jain <sahiljain5040@gmail.com>
2018-08-11 17:54:38 +0530

    Replaced function calls with Lambda and method reference

--- data/src/main/java/com/demo/data/search/repository/RestaurantApiRepository.java
+++ data/src/main/java/com/demo/data/search/repository/RestaurantApiRepository.java
@@ -2,9 +2,9 @@ package com.demo.data.search.repository;
 
 import com.demo.data.search.network.api.SearchApi;
 import com.demo.data.search.network.response.ApiSearchRestaurantsResponse;
+import com.demo.domain.base.Constants;
 import com.demo.domain.search.model.RestaurantWrapper;
 import com.demo.domain.search.repository.RestaurantRepository;
-import com.demo.domain.base.Constants;
 
 import java.util.List;
 import java.util.Map;
@@ -12,7 +12,6 @@ import java.util.Map;
 import javax.inject.Inject;
 
 import io.reactivex.Observable;
-import io.reactivex.functions.Function;
 
 /**
  * Created by sahil on 3/17/18.
@@ -29,11 +28,6 @@ public class RestaurantApiRepository implements RestaurantRepository {
     @Override
     public Observable<List<RestaurantWrapper>> getRestaurants(Map<String, String> queryParams) {
         return mSearchApi.getRestaurantsObservable(Constants.API_KEY, queryParams)
-                .map(new Function<ApiSearchRestaurantsResponse, List<RestaurantWrapper>>() {
-                    @Override
-                    public List<RestaurantWrapper> apply(ApiSearchRestaurantsResponse apiSearchRestaurantsResponse) throws Exception {
-                        return apiSearchRestaurantsResponse.getRestaurants();
-                    }
-                });
+                .map(ApiSearchRestaurantsResponse::getRestaurants);
     }
 }
